fcf73546b3a1cde9e332e5f022f3ac7b39b65ac8
thestonage <iconofthestoneage@gmail.com>
2010-05-09 21:40:21 +0200

    Fixed Vector Tests

--- test/vector_test.js
+++ test/vector_test.js
@@ -1,38 +1,38 @@
-VectorTestCase = TestCase("Simple");
+VectorTestCase = TestCase("Vector");
 
 VectorTestCase.prototype.testEquality = function () {
-	assertEquals(new VectorTestCase(1, 1), new VectorTestCase(1, 1));
-	assertNotEquals(new VectorTestCase(1, 1), new VectorTestCase(1, 2));
+	assertEquals(new Vector(1, 1), new Vector(1, 1));
+	assertNotEquals(new Vector(1, 1), new Vector(1, 2));
 }
 
 VectorTestCase.prototype.testAdd = function() {
-	assertEquals((new VectorTestCase(1, 2)).add(new VectorTestCase(1, 3)), new VectorTestCase(2, 5));
-	assertEquals((new VectorTestCase(1, 1)).add(new VectorTestCase(0, 0)), new VectorTestCase(1, 1));
+	assertEquals((new Vector(1, 2)).add(new Vector(1, 3)), new Vector(2, 5));
+	assertEquals((new Vector(1, 1)).add(new Vector(0, 0)), new Vector(1, 1));
 };
 
 VectorTestCase.prototype.testSub = function() {
-	assertEquals((new VectorTestCase(2, 5)).sub(new VectorTestCase(1, 3)), new VectorTestCase(1, 2));
-	assertEquals((new VectorTestCase(1, 1)).sub(new VectorTestCase(0, 0)), new VectorTestCase(1, 1));
-	assertEquals((new VectorTestCase(0, 0)).sub(new VectorTestCase(1, 1)), new VectorTestCase(-1, -1));
+	assertEquals((new Vector(2, 5)).sub(new Vector(1, 3)), new Vector(1, 2));
+	assertEquals((new Vector(1, 1)).sub(new Vector(0, 0)), new Vector(1, 1));
+	assertEquals((new Vector(0, 0)).sub(new Vector(1, 1)), new Vector(-1, -1));
 };
 
 VectorTestCase.prototype.testMul = function() {
-	assertEquals(new VectorTestCase(3, 2).mul(2), new VectorTestCase(6, 4));
+	assertEquals(new Vector(3, 2).mul(2), new Vector(6, 4));
 };
 
 VectorTestCase.prototype.testDiv = function() {
-	assertEquals(new VectorTestCase(2, 4).div(2), new VectorTestCase(1, 2));
+	assertEquals(new Vector(2, 4).div(2), new Vector(1, 2));
 };
 
 VectorTestCase.prototype.testLen = function() {
-	assertEquals(new VectorTestCase(1, 0).len(), 1);
-	assertEquals(new VectorTestCase(0, 1).len(), 1);
-	assertEquals(new VectorTestCase(2, 1).len(), Math.sqrt(5));
+	assertEquals(new Vector(1, 0).len(), 1);
+	assertEquals(new Vector(0, 1).len(), 1);
+	assertEquals(new Vector(2, 1).len(), Math.sqrt(5));
 };
 
 VectorTestCase.prototype.testNormalize = function() {
-	assertEquals(new VectorTestCase(1, 0).normalize(), new VectorTestCase(1, 0));
-	assertEquals(new VectorTestCase(3, 0).normalize(), new VectorTestCase(1, 0));
+	assertEquals(new Vector(1, 0).normalize(), new Vector(1, 0));
+	assertEquals(new Vector(3, 0).normalize(), new Vector(1, 0));
 };
 
 
