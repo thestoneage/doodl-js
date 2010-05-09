923c87183536292d089039f25951b112e54661a3
thestonage <iconofthestoneage@gmail.com>
2010-05-09 19:06:09 +0200

    Pulled Vector out of layout.js and added tests

---
+++ test/vector_test.js
@@ -0,0 +1,38 @@
+VectorTestCase = TestCase("Simple");
+
+VectorTestCase.prototype.testEquality = function () {
+	assertEquals(new VectorTestCase(1, 1), new VectorTestCase(1, 1));
+	assertNotEquals(new VectorTestCase(1, 1), new VectorTestCase(1, 2));
+}
+
+VectorTestCase.prototype.testAdd = function() {
+	assertEquals((new VectorTestCase(1, 2)).add(new VectorTestCase(1, 3)), new VectorTestCase(2, 5));
+	assertEquals((new VectorTestCase(1, 1)).add(new VectorTestCase(0, 0)), new VectorTestCase(1, 1));
+};
+
+VectorTestCase.prototype.testSub = function() {
+	assertEquals((new VectorTestCase(2, 5)).sub(new VectorTestCase(1, 3)), new VectorTestCase(1, 2));
+	assertEquals((new VectorTestCase(1, 1)).sub(new VectorTestCase(0, 0)), new VectorTestCase(1, 1));
+	assertEquals((new VectorTestCase(0, 0)).sub(new VectorTestCase(1, 1)), new VectorTestCase(-1, -1));
+};
+
+VectorTestCase.prototype.testMul = function() {
+	assertEquals(new VectorTestCase(3, 2).mul(2), new VectorTestCase(6, 4));
+};
+
+VectorTestCase.prototype.testDiv = function() {
+	assertEquals(new VectorTestCase(2, 4).div(2), new VectorTestCase(1, 2));
+};
+
+VectorTestCase.prototype.testLength = function() {
+	assertEquals(new VectorTestCase(1, 0).length(), 1);
+	assertEquals(new VectorTestCase(0, 1).length(), 1);
+	assertEquals(new VectorTestCase(2, 1).length(), Math.sqrt(5));
+};
+
+VectorTestCase.prototype.testNormalize = function() {
+	assertEquals(new VectorTestCase(1, 0).normalize(), new VectorTestCase(1, 0));
+	assertEquals(new VectorTestCase(3, 0).normalize(), new VectorTestCase(1, 0));
+};
+
+
